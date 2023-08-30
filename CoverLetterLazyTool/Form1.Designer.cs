23debae15601a7395fdc6a601979256984794dee
liyue <liyue@FAKEFORRESTSTD>
2023-08-30 01:02:32 -0400

    Add project files.

---
+++ CoverLetterLazyTool/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CoverLetterLazyTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(216, 1088);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Doc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(216, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1310, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(216, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1310, 38);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Company Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Occupation";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(216, 137);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1310, 910);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Content";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(482, 1088);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 53);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Insert Letter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(742, 1088);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 53);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Substitue Text";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1020, 1088);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 53);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Save File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1589, 1259);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CoverLetterLazyTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CoverLetterLazyTool/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Interop.Word;
+
+namespace CoverLetterLazyTool
+{
+    public partial class Form1 : Form
+    {
+        private Microsoft.Office.Interop.Word.Application wordApp;
+        private Document document;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FindAndReplace(string searchText, string replacementText)
+        {
+            foreach (Range range in document.StoryRanges)
+            {
+                range.Find.ClearFormatting();
+                range.Find.Execute(FindText: searchText, ReplaceWith: replacementText, Replace: WdReplace.wdReplaceAll);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wordApp = new Microsoft.Office.Interop.Word.Application();
+            document = wordApp.Documents.Add();
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            MessageBox.Show("Document created. You can start adding content.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (document != null)
+            {
+                if (textBox3.Text != "")
+                {
+                    string paragraphText = textBox3.Text;
+
+                    // Insert the paragraph into the document
+                    Paragraph para = document.Content.Paragraphs.Add();
+                    para.Range.Text = paragraphText;
+
+                    // Move to the next line for the next paragraph
+                    document.Content.InsertParagraphAfter();
+
+                    // Clear the textbox for the next input
+                    //textBox3.Clear();
+
+                    MessageBox.Show("Paragraph added successfully!");
+                }
+                else
+                {
+                    textBox3.Focus();
+                    MessageBox.Show("Your paragraph added should not be empty!\n Please try again!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please create a document first.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (document != null)
+            {
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    textBox1.Focus();
+                    MessageBox.Show("Either the company name or the occupation is not supposed to be empty,\n Please try again!");
+                }
+                string searchText1 = "[CompanyName]";
+                string replacementText1 = textBox1.Text;
+
+                string searchText2 = "[Occupation]";
+                string replacementText2 = textBox2.Text;
+
+                FindAndReplace(searchText1, replacementText1);
+                FindAndReplace(searchText2, replacementText2);
+
+                MessageBox.Show("Word replaced successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Please create a document first.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (document != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Word Document (*.docx)|*.docx";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    document.SaveAs2(filePath);
+                    document.Close();
+                    wordApp.Quit();
+                    MessageBox.Show($"Document exported and saved to {filePath}");
+                    button1.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please create a document first.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+    }
+}
